0f7f10cdc8ce126be67fe58ca07edf37a9be6e9c
Miyajima Takaki <takaki.mz@gmail.com>
2021-12-26 16:44:41 -0800

    add: make Header comp and embeded

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
-import sveltePreprocess from 'svelte-preprocess';
+// import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,7 +42,7 @@ export default {
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess({ postcss: true }),
-			preprocess: sveltePreprocess(),
+			// preprocess: sveltePreprocess(),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
