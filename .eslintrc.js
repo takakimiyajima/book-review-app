c0bfcbd5b120d7c3c61d457fbbf9ab2d155206ba
Miyajima Takaki <takaki.mz@gmail.com>
2021-12-27 18:20:42 -0800

    chore: eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  rules: {
+    // セミコロンない場合、エラー出力
+    "prefer-const": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "max-len": ["error", { "code": 90, "ignoreComments": true }]
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    // ignore style tags in Svelte because of Tailwind CSS
+    // See https://github.com/sveltejs/eslint-plugin-svelte3/issues/70
+    'svelte3/ignore-styles': () => true
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['node_modules']
+}
