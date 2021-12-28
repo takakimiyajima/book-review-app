0da441b766a24ca78e91159af15beee32440253a
Miyajima Takaki <takaki.mz@gmail.com>
2021-12-27 19:33:18 -0800

    feat: can value bind

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,7 +1,13 @@
+<script lang="ts">
+  // NOTE: Can set defalt value if not get value
+  export let value = ''
+</script>
+
 <div class="shadow flex">
-  <input class="w-full rounded p-2" type="text" placeholder="Search...">
+  <input bind:value class="w-full rounded p-2" type="text" placeholder="Search...">
   <button class="
-    bg-white w-auto
+    bg-white
+    w-auto
     flex
     justify-end
     items-center
@@ -9,7 +15,15 @@
     p-2
     hover:text-blue-400
   ">
-    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
+    <svg
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+      class="w-6 h-6"
+    >
       <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
     </svg>
   </button>
--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import SearchBar from '@/components/SearchBar.svelte'
+
+  /** State */
+  let q = 'JavaScript'
 </script>
 
 <form>
-  <SearchBar />
+  <SearchBar bind:value={q} />
 </form>
+<div class="mt-4 text-center">{ q }</div>
