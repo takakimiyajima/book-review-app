c5f767ce432f96e1edf0ee7f30be9f3dd5d2c796
Miyajima Takaki <takaki.mz@gmail.com>
2021-12-27 20:05:59 -0800

    fix: gather file

--- src/repositories/book/BookRepository.ts
+++ src/repositories/book/BookRepository.ts
@@ -1,14 +1,55 @@
-import { BookItem, IBookRepository, Params, Response } from './types'
-import { httpClient } from '@/repositories/httpClient'
+import axios from '@/repositories/httpClient'
 
-export class BookRepository implements IBookRepository {
+/**
+ * Response of Google Books API
+ */
+ export type Response = {
+  items: BookItem[];
+  kind: string;
+  totalItems: number;
+}
+
+/**
+ * 本の情報
+ */
+ export type BookItem = {
+  id: string;
+  volumeInfo: {
+    title: string;
+    authors?: string[];
+    publishedDate?: string;
+    description?: string;
+    publisher?: string;
+    imageLinks?: {
+      smallThumbnail: string;
+      thumbnail: string;
+    };
+    pageCount: number;
+    previewLink?: string;
+  };
+  saleInfo?: {
+    listPrice: {
+      amount: number;
+    };
+  };
+}
+
+/**
+ * query parameters
+ */
+export type Params = {
+  q: string;
+  startIndex?: number;
+}
+
+export class BookRepository {
   async get(params: Params) {
-    const { data } = await httpClient.get<Response>('/', { params })
+    const { data } = await axios.get<Response>('/', { params })
     return data
   }
 
   async find(id: string) {
-    const { data } = await httpClient.get<BookItem>(`/${id}`)
+    const { data } = await axios.get<BookItem>(`/${id}`)
     return data
   }
 }
